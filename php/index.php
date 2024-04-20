85bbbb73a89af794195f45b91977787c0be8282f
xxrtemy <rebelgamechannel@gmail.com>
2024-04-20 13:25:30 +0300

    completed mailer

--- php/index.php
+++ php/index.php
@@ -4,11 +4,11 @@ require_once '../vendor/autoload.php';
 $settings = require_once __DIR__ . '/mail_config.php';
 require_once __DIR__ . '/mailer.php';
 
-$body = "<h1>Письмо с сайта</h1>\n
+$body = "<h1>Данные пользователя</h1>\n
 <p><strong>Телефон:</strong> " .$_POST['phone']."</p>";
 
 
-if(!send_mail($settings['mail_settings_prod'], ['rebelgamechannel@gmail.com'], 'Заявка Михерев', $body)){
+if(!send_mail($settings['mail_settings_prod'], ['ya.alexgolubev@yandex.ru'], 'Заявка Михерев', $body)){
     $message = 'Ошибка';
 }else{
     $message = 'Данные отправлены';
